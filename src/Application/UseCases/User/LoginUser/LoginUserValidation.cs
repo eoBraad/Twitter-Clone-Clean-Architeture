028811b984e674c35f631449dae7ebbf63ca20c3
EoBrad <joao.alencar099@gmail.com>
2024-02-01 12:24:05 -0300

    Login user Validation created

--- src/Application/UseCases/User/LoginUser/LoginUserValidation.cs
+++ src/Application/UseCases/User/LoginUser/LoginUserValidation.cs
@@ -1,6 +1,15 @@
-﻿namespace Application.UseCases.User.LoginUser;
+﻿using communication.Requests.User;
+using FluentValidation;
 
-public class LoginUserValidation_
+namespace Application.UseCases.User.LoginUser;
+
+public class LoginUserValidation : AbstractValidator<LoginUserRequest>
 {
+    public LoginUserValidation()
+    {
+        When(x => x.Credentials.First() != '@', () => RuleFor(x => x.Credentials).EmailAddress());
+        When(x => x.Credentials.First() == '@', () => RuleFor(x => x.Credentials).NotNull().MinimumLength(3).MaximumLength(100));
+        RuleFor(x => x.Password).NotNull().MinimumLength(3).MaximumLength(100);
+    }
 
 }
