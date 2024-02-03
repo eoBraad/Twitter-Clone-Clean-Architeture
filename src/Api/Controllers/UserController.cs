350f6eea80bb6c168d761db81f6ac7e8d38009da
EoBrad <joao.alencar099@gmail.com>
2024-02-02 22:52:34 -0300

    refact controller

--- src/Api/Controllers/UserController.cs
+++ src/Api/Controllers/UserController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Api.Controllers;
 
 [ApiController]
-[Route("api/v1/user")]
+[Route("api/v1/[controller]")]
 public class UserController : ControllerBase
 {
     [HttpPost]
